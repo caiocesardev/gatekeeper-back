ffd370fb324099d5b4ac0b1e71c17a9b609a18d0
Leonardo E. Silva <42047532+leoesilva@users.noreply.github.com>
2026-06-01 15:54:15 -0300

    initialize project structure with Spring Boot, Docker setup, and basic configurations

---
+++ src/main/kotlin/com/webcrafters/gatekeeperback/GatekeeperBackApplication.kt
@@ -0,0 +1,11 @@
+package com.webcrafters.gatekeeperback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatekeeperBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<GatekeeperBackApplication>(*args)
+}
